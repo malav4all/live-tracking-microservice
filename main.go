a121957c74252a675525aadb212a957d54b4096b
malav <malav.naagar@imzcorporate.com>
2024-08-08 12:59:06 +0530

    some chnages

--- main.go
+++ main.go
@@ -125,6 +125,6 @@ func main() {
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 	)(r)
 
-	log.Println("Starting server on :9090...")
-	log.Fatal(http.ListenAndServe(":9090", corsHandler))
+	log.Println("Starting server on :6000...")
+	log.Fatal(http.ListenAndServe(":6000", corsHandler))
 }
